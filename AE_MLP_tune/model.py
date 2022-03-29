7c5fd778393556f90fa26d9cf678b41620fc4712
nonoy <171913146@qq.com>
2022-03-29 17:27:39 +0800

    添加了ae-mlp模型的代码

---
+++ AE_MLP_tune/model.py
@@ -0,0 +1,87 @@
+import os
+import torch
+from torch import nn
+import pytorch_lightning as pl
+from util.nn_util import init_rnn_hidden
+from interface.abstract_model import AbsModel
+
+
+class AeMlpModel(AbsModel):
+    '''
+        进行gru神经网络预测的测试
+    '''
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.lr = config['running.lr']  # configure_optimizers使用
+        self.encoder = Encoder(config)
+        self.decoder = Decoder(config)
+
+    def forward(self, x: torch.Tensor):
+        encoded = self.encoder(x)
+        # cat_code = torch.cat(encoded, dim=1)
+        out = self.decoder(encoded)
+        return out
+
+
+class Mlp_Uint(nn.Module):
+    '''
+         进行gru神经网络预测的测试
+     '''
+
+    def __init__(self, config, input_size=None, layer1=None, layer2=None, layer3=None, out_size=None):
+        super().__init__()
+        self.config = config
+        input_size = input_size if input_size else config['common.history_seq_len']
+        hidden_num_1 = layer1 if layer1 else config['unit.layer1.hidden_num']
+        hidden_num_2 = layer2 if layer2 else config['unit.layer2.hidden_num']
+        hidden_num_3 = layer3 if layer3 else config['unit.layer3.hidden_num']
+        output_num = out_size if out_size else config['aemlp.encode_size']
+        self.mlp = nn.Sequential(
+            nn.Linear(input_size, hidden_num_1),
+            nn.ELU(),
+            nn.Linear(hidden_num_1, hidden_num_2),
+            nn.ELU(),
+            nn.Linear(hidden_num_2, hidden_num_3),
+            nn.ELU(),
+            nn.Linear(hidden_num_3, output_num)
+        )
+
+    def forward(self, x: torch.Tensor):
+        # h_n of shape (num_layers * num_directions, batch, hidden_size)
+        # x原始维度，batch,seq_len,input_dim ，input_dim==1
+        out = self.mlp(x)
+        return out
+
+
+class Encoder(nn.Module):
+    def __init__(self, config):
+        super(Encoder, self).__init__()
+        self.config = config
+        self.layer1 = Mlp_Uint(config)
+        self.layer2 = Mlp_Uint(config)
+        self.layer3 = Mlp_Uint(config)
+        self.layer4 = Mlp_Uint(config)
+
+    def forward(self, x):
+        out1 = self.layer1(x[:, 0, :])
+        out2 = self.layer2(x[:, 1, :])
+        out3 = self.layer3(x[:, 2, :])
+        out4 = self.layer4(x[:, 3, :])
+        return torch.cat((out1, out2, out3, out4), dim=1)
+
+
+class Decoder(nn.Module):
+    def __init__(self, config):
+        super(Decoder, self).__init__()
+        self.config = config
+        self.mlp = Mlp_Uint(config,
+                            input_size=config['aemlp.encode_size'] * 4,
+                            layer1=config['de.layer1'],
+                            layer2=config['de.layer2'],
+                            layer3=config['de.layer3'],
+                            out_size=1)
+
+    def forward(self, x):
+        return self.mlp(x)
---
+++ AE_MLP_tune/run_predict.py
@@ -0,0 +1,125 @@
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
+from pytorch_lightning.loggers import TensorBoardLogger
+from ray import tune
+from ray.tune import CLIReporter, ExperimentAnalysis
+from ray.tune.schedulers import ASHAScheduler, PopulationBasedTraining
+from ray.tune.integration.pytorch_lightning import TuneReportCallback, \
+    TuneReportCheckpointCallback
+import ray
+from ray import tune
+
+from util import *
+
+from pytorch_lightning import seed_everything
+
+seed_everything(2022, workers=True)
+
+from model import AeMlpModel as BasicModel
+from common_dataloader.mutilple_loader import MutilSeqDataModule
+from config import parameter
+from common_tune.tune_config import trial_name_string, trial_dirname_creator
+from util import *
+
+
+def prepare_daloader(config=parameter, rows=3240):
+    path = r'../data/Apt2_2015_hour_weather_bfill.xlsx'
+    col_list = ['power', 'temperature', 'humidity', 'dewPoint']
+    table = easy_read_data(path).iloc[:rows, :][col_list]
+    parameter['mlp.input_size'] = config['common.history_seq_len'] * len(col_list)
+    sc_table, sc_list = easy_mutil_transformer(table, [])
+    # data
+    dataloader = MutilSeqDataModule(sc_table, 'power',
+                                    history_seq_len=parameter['common.history_seq_len'],
+                                    batch_size=parameter['running.batch_size'])
+    return dataloader
+
+
+def lightning_run(config, dataloader, checkpoint_dir=None):
+    model = BasicModel(config)
+    # training
+    trainer = pl.Trainer(
+        gpus=parents_config['gpu'],
+        fast_dev_run=True,  # 检查程序完整性时候执行
+        # limit_train_batches=0.3,
+        # limit_val_batches=0.3,
+        # limit_test_batches=0.5,
+        val_check_interval=0.5,
+        # gradient_clip_val=0.3,  # 梯度裁剪
+        max_epochs=parameter['running.max_epoch'],
+        enable_progress_bar=False,
+        logger=TensorBoardLogger(
+            save_dir=tune.get_trial_dir(), name="", version="."),
+        callbacks=[
+            ModelCheckpoint(monitor='val_loss'),  # 记录验证loss
+            # EarlyStopping(monitor="val_loss", mode="min")
+            # TuneReportCallback(
+            #     {
+            #         "v_loss": "test_loss",
+            #         # "p_loss": "predict_loss"
+            #     },
+            #     on="test_end")
+            TuneReportCheckpointCallback(
+                metrics={
+                    "v_loss": "test_loss",
+                },
+                filename="checkpoint",
+                on="test_end")
+        ]
+    )
+    trainer.fit(model, dataloader)
+    trainer.test(model, dataloader)
+    # trainer.predict(model, dataloader)
+
+
+def tune_train(dataloader):
+    ray.init(local_mode=True)
+    #ray.init()
+    scheduler = ASHAScheduler(
+        max_t=10,
+        grace_period=1,
+        reduction_factor=2)
+    reporter = CLIReporter(
+        max_report_frequency=30,
+        print_intermediate_tables=False,
+        # parameter_columns=[],
+        metric_columns=["v_loss"])
+
+    train_fn_with_parameters = tune.with_parameters(lightning_run,
+                                                    dataloader=dataloader
+                                                    )
+
+    resources_per_trial = {"cpu": 1, "gpu": 0}
+    analysis = tune.run(train_fn_with_parameters,
+                        resources_per_trial=resources_per_trial,
+                        metric="v_loss",
+                        mode="min",
+                        config=parameter,
+                        num_samples=1,
+                        # scheduler=scheduler,
+                        progress_reporter=reporter,
+                        trial_name_creator=trial_name_string,
+                        trial_dirname_creator=trial_dirname_creator,
+                        local_dir='./ray_results',
+                        name="AE_MLP_tune",
+                        )
+
+    print("Best hyperparameters found were: ", analysis.best_config)
+    print("Best result", analysis.best_result_df)
+    return analysis
+
+
+if __name__ == '__main__':
+    import warnings
+    warnings.filterwarnings('ignore')
+
+    dataloader = prepare_daloader()
+    result = tune_train(dataloader)
+    # 找到最佳配置和文件，恢复模型，进行预测
+    ckp = "{}/{}".format(result.best_checkpoint, 'checkpoint')
+    best_config = result.get_best_config()
+    bmodel = BasicModel.load_from_checkpoint(checkpoint_path=ckp, config=best_config)
+    trainer = pl.Trainer(gpus=parents_config['gpu'])
+    trainer.predict(bmodel, dataloader)
