e4315fafece4d3c1130a8b4282d9f6efde76dacf
nonoy <171913146@qq.com>
2022-04-07 11:05:02 +0800

    predict util 加载模型函数

--- util/predict_util.py
+++ util/predict_util.py
@@ -1,7 +1,20 @@
 from util import *
+from ray.tune import ExperimentAnalysis
 
 
-def get_predict_data_tables(path:str,col_list,train_rate=0.9, rows=2160):
+# 从文件结果中加载训练好的最佳模型
+def load_model(exp_path, model):
+    exp = ExperimentAnalysis(
+        default_metric='v_loss',
+        default_mode='min',
+        experiment_checkpoint_path=exp_path)
+    ckp = "{}/{}".format(exp.best_checkpoint, 'checkpoint')
+    best_config = exp.get_best_config()
+    bmodel = model.load_from_checkpoint(checkpoint_path=ckp, config=best_config)
+    return bmodel, exp
+
+
+def get_predict_data_tables(path: str, col_list, train_rate=0.9, rows=2160):
     table = easy_read_data(path).iloc[:rows, :][col_list]
     sc_table, sc_list = easy_mutil_transformer(table, [])
     # data
